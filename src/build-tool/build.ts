482ffc09446f06923b572ce81e27151bf236f84a
Dariusz <dariusz@Dariuszs-MacBook-Pro.local>
2018-04-15 01:55:05 +0200

    prevent race condition

--- src/build-tool/build.ts
+++ src/build-tool/build.ts
@@ -5,11 +5,16 @@ import * as child from 'child_process';
 import * as fs from 'fs';
 import * as fse from 'fs-extra';
 import * as glob from "glob";
-import * as cpr from 'cpr'
 import * as dateformat from "dateformat";
+
+const ps = require('ps-node');
+const sleep = require('sleep')
 
 import { CodeTransform } from './isomorphic';
 import { Helpers } from './helpers';
+
+const lockfile = path.join(process.cwd(), 'tmp-lockfile-isomorphic-build.txt')
+type ProcessInfo = { arguments: string; pid: string; command: string; ppid: string; }
 
 export interface BuildPathes {
   foldersPathes?: {
@@ -20,6 +25,7 @@ export interface BuildPathes {
   };
   toolsPathes?: {
     tsc: string;
+    morphi: string;
   };
   build?: {
     otherIsomorphicLibs?: string[];
@@ -30,21 +36,51 @@ function date() {
   return `[${dateformat(new Date(), 'HH:MM:ss')}]`;
 }
 
-export function wrapperIsomorphicBuildProcess(options?: BuildPathes) {
+function removeLockfile() {
+  if (fs.existsSync(lockfile)) {
+    // console.log('REMOVE LOCKIE FILE')
+    fs.unlinkSync(lockfile)
+  }
+}
+
+export async function wrapperIsomorphicBuildProcess(options?: BuildPathes) {
   try {
     console.log(`${date()} Building server/browser version of ${path.basename(process.cwd())}...`)
     child.execSync(`${options.toolsPathes.tsc} --outDir ${options.foldersPathes.dist}`)
-    buildIsomorphicVersion(options);
+    await buildIsomorphicVersion(options);
+
     console.log(`${date()} Typescript compilation OK`)
   } catch (error) {
+    removeLockfile()
     console.log(error);
     console.error(`${date()} Typescript compilation ERROR`)
     process.exit(0)
   }
 }
 
+function isCurrent(pid: number, command = 'morphi'): boolean {
+  const i: ProcessInfo = JSON.parse(child.execSync(`${command} process-info ${pid}`).toString())
+  // console.log(`CHALENGER ppid`, i.ppid)
+  return (process.ppid == Number(i.ppid))  // (pid === parseInt(i.pid)) && (i.command === 'node');
+}
 
-export function buildIsomorphicVersion(options?: BuildPathes) {
+function preventRaceCodition(morphiCommand = 'morphi') {
+  // console.log(`OWN PID ${process.pid}`)
+  // console.log(`OWN ppid ${process.ppid}`)
+  if (fs.existsSync(lockfile)) {
+    const pid = parseInt(fs.readFileSync(lockfile, 'utf8').toString());
+    if (isCurrent(pid, morphiCommand)) {
+      console.log(`From same process.. ${pid} killing...`)
+      process.kill(pid)
+    }
+    fs.writeFileSync(lockfile, process.pid, 'utf8');
+  } else {
+    fs.writeFileSync(lockfile, process.pid, 'utf8');
+  }
+}
+
+export async function buildIsomorphicVersion(options?: BuildPathes) {
+
   if (!options) options = {} as any;
   const { foldersPathes = {}, toolsPathes = {}, build = {} } = options;
   const FOLDER = _.merge({
@@ -56,6 +92,7 @@ export function buildIsomorphicVersion(options?: BuildPathes) {
       browser: 'tsconfig.browser.json',
       default: 'tsconfig.json'
     }
+
   }, foldersPathes);
 
   const dist = path.join(process.cwd(), FOLDER.dist)
@@ -63,13 +100,21 @@ export function buildIsomorphicVersion(options?: BuildPathes) {
   const tmpSrc = path.join(process.cwd(), FOLDER.tmpSrc)
   const src = path.join(process.cwd(), FOLDER.src)
 
+
+
   const TOOLS = _.merge({
-    tsc: 'npm-run tsc'
+    tsc: 'npm-run tsc',
+    morphi: 'morphi'
   }, toolsPathes);
   const BUILD = _.merge({
     otherIsomorphicLibs: []
   }, build);
   BUILD
+
+  preventRaceCodition(TOOLS.morphi);
+  // console.log('wainting ')
+  // sleep.sleep(1000)
+  // console.log('AAAAA')
 
   // browser folder
   if (fs.existsSync(browser)) {
@@ -96,6 +141,6 @@ export function buildIsomorphicVersion(options?: BuildPathes) {
   child.execSync(`${TOOLS.tsc} --outDir ../${FOLDER.dist}/${FOLDER.browser}`, { stdio: [0, 1, 2], cwd: tempSrc })
 
   child.execSync(Helpers.createLink('.', path.join(dist, FOLDER.browser)))
-
+  removeLockfile()
 }
 
--- src/build-tool/index.ts
+++ src/build-tool/index.ts
@@ -3,6 +3,7 @@ import * as child from 'child_process';
 import * as path from 'path';
 
 import * as fs from 'fs';
+const ps = require('ps-node');
 
 import { Helpers } from './helpers';
 import { wrapperIsomorphicBuildProcess } from "./build";
@@ -16,7 +17,7 @@ export function run(argsv: string[], morphiEnvironmentCheck = true) {
     Helpers.checkEnvironment()
   }
   if (argsv.length >= 3) {
-    const commandName: 'build' | 'ln' | 'new' | '-v' | '-h' | '--help' | '-help' = argsv[2] as any;
+    const commandName: 'build' | 'ln' | 'new' | 'process-info' | '-v' | '-h' | '--help' | '-help' = argsv[2] as any;
 
 
     if (commandName === 'build') {
@@ -25,10 +26,31 @@ export function run(argsv: string[], morphiEnvironmentCheck = true) {
           otherIsomorphicLibs: argsv.slice(4)
         }
       });
+      process.exit(0)
+    } else if (commandName === 'process-info') {
+      // A simple pid lookup
+      if (!Array.isArray(argsv) || argsv.length < 2) {
+        console.log(`To few arguments for process-info function...`);
+        process.exit(0)
+      }
+      const pid = argsv[3];
+      ps.lookup({ pid }, function (err, resultList) {
+        if (err) {
+          throw new Error(err);
+        }
+        const process = resultList[0];
+        if (process) {
+          console.log(JSON.stringify(process));
+        }
+        else {
+          console.log(JSON.stringify({}));
+        }
+      });
     } else if (commandName === '-h' || commandName === '-help' || commandName === '--help') {
       console.log('Usage: morphi build <lib1> <lib2> ... ')
       console.log('Where lib(n) is name of included in node_modules isomorphic lib')
       console.log('Example isomorphic libs are: typeorm, ng2-rest, morphi...');
+      process.exit(0)
     } else if (commandName === 'ln') {
       if (!Array.isArray(argsv) || argsv.length < 2) {
         console.log(`To few arguments for linking function...`);
@@ -37,6 +59,7 @@ export function run(argsv: string[], morphiEnvironmentCheck = true) {
       const link = argsv[3]
       let target = argsv[4]
       child.execSync(Helpers.createLink(target, link))
+      process.exit(0)
     } else if (commandName === 'new') {
       if (!Array.isArray(argsv) || argsv.length < 4) {
         console.log(`To few arguments.. try: morphi new myAppName`);
@@ -44,10 +67,12 @@ export function run(argsv: string[], morphiEnvironmentCheck = true) {
       }
       const name = argsv[3]
       copyExampleTo(name)
+      process.exit(0)
     } else if (commandName === '-v') {
       console.log(JSON.parse(fs.readFileSync(path.join(__dirname, '../..', 'package.json').toString(), 'utf8').toString()).version)
+      process.exit(0)
     }
-    process.exit(0)
+
 
   }
 
