c53e0a33d70addb76c55c18b4e9677b308afae1b
Dariusz Filipiak <darekf77@gmail.com>
2018-08-01 14:52:35 +0200

    amazing form in progress!

--- src/fromly.ts
+++ src/fromly.ts
@@ -1,18 +1,38 @@
 
 import { SYMBOL } from "./SYMBOLS";
 import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+import { Describer } from './helpers';
+import * as _ from 'lodash';
 
 
-export function FormlyForm(fromFn: (from: FormlyFieldConfig[]) => FormlyFieldConfig[]) {
+export function FormlyForm(fromFn?: (from: FormlyFieldConfig[]) => FormlyFieldConfig[]) {
   return function (target: Function) {
     if (!target[SYMBOL.FORMLY_METADATA]) {
       target[SYMBOL.FORMLY_METADATA] = [];
+    }
+    if (typeof target === 'function') {
+      target[SYMBOL.FORMLY_METADATA] = Describer.describe(target).map(propName => {
+
+        let type = 'input';
+
+
+        return {
+          key: propName,
+          type,
+          templateOptions: {
+            label: _.toUpper(_.kebabCase(propName))
+          }
+        }
+      })
+    }
+    if (typeof fromFn === 'function') {
+      target[SYMBOL.FORMLY_METADATA] = fromFn(target[SYMBOL.FORMLY_METADATA]);
     }
   }
 }
 
 export function getFromlyFrom(entity: Function) {
-
+  return entity && entity[SYMBOL.FORMLY_METADATA];
 }
 
 
