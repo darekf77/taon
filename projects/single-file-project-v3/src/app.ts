a6b4df5b67e3a892d0bcf15ff376e5de71aaf79b
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-02-27 02:18:10 +0100

    before v3 finish

--- projects/single-file-project-v3/src/app.ts
+++ projects/single-file-project-v3/src/app.ts
@@ -1,93 +1,10 @@
-import { Morphi } from 'morphi'
 
-declare const ENV;
-console.log('ENV', ENV);
 
-@Morphi.Entity()
-export class USER {
-
-  singYourName() {
-    console.log(`helellelleoeo ${this.name}`)
-  }
-  name: string;
+async function start() {
+    console.log('HELLO')
 }
 
-
-@Morphi.Controller()
-class UserController {
-
-  @Morphi.Http.GET()
-  hello(@Morphi.Http.Param.Query('config') config?: any): Morphi.Response<string> {
-    //#region @backendFunc
-    return async () => {
-
-      return 'this is cool haha !'
-    }
-    //#endregion
-  }
-
-  @Morphi.Http.GET()
-  getEntity(): Morphi.Response<USER> {
-    //#region @backendFunc
-    return async () => {
-      let u = new USER();
-      u.name = 'test '
-      return u;
-    }
-    //#endregion
-  }
-
-}
-
-const host = 'http://localhost:3000'
-const controllers: Morphi.Base.Controller<any>[] = [UserController as any];
-
-
-const start = async () => {
-
-  //#region @backend
-  const config = {
-    type: "sqlite",
-    database: 'tmp-db.sqlite',
-    synchronize: true,
-    dropSchema: true,
-    logging: false
-  } as any;
-  //#endregion
-
-
-  const context = await Morphi.init({
-    host,
-    controllers,
-    //#region @backend
-    config
-    //#endregion
-  });
-  console.log(context);
-  if (Morphi.IsBrowser) {
-
-    const body: HTMLElement = document.getElementsByTagName('body')[0];
-    let test = new UserController()
-    test.hello({ siema: 'siema' }).received.observable.subscribe(dataFromBackend => {
-      body.innerHTML = `<h1>${dataFromBackend.body.text}</h1>`;
-    });
-
-    const helloData = await test.hello().received
-    console.log('Realtime hsould not be inited', helloData.body.text)
-
-
-    test.getEntity().received.observable.subscribe(user => {
-      const u = user.body.json;
-      console.log('USER', u)
-      u.singYourName()
-    })
-
-  }
-
-}
-
-
-if (Morphi.IsBrowser) {
+if (typeof window !== 'undefined') {
   start()
 }
 
