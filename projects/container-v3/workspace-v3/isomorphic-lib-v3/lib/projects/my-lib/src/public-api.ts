385f30b4cd89dc3f50ac4597b581cdad83565119
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-02-28 19:26:35 +0100

    working on lib v3 build

--- projects/container-v3/workspace-v3/isomorphic-lib-v3/lib/projects/my-lib/src/public-api.ts
+++ projects/container-v3/workspace-v3/isomorphic-lib-v3/lib/projects/my-lib/src/public-api.ts
@@ -1,7 +1,1 @@
-/*
- * Public API Surface of my-lib
- */
-
-export * from './lib/my-lib.service';
-export * from './lib/my-lib.component';
-export * from './lib/my-lib.module';
+export * from './lib';
