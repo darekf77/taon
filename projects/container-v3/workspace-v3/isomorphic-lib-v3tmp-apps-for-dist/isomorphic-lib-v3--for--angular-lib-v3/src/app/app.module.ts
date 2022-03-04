59758c19dda88abcec2724f102b4dd22bc3ec8b2
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-03-04 23:47:34 +0100

    before app build

--- projects/container-v3/workspace-v3/isomorphic-lib-v3tmp-apps-for-dist/isomorphic-lib-v3--for--angular-lib-v3/src/app/app.module.ts
+++
@@ -1,6 +0,0 @@
-
-import { Isomorphic-lib-v-3 } from './isomorphic-lib-v3';
-import { Isomorphic-lib-v-3 } from './isomorphic-lib-v3';
-import { Isomorphic-lib-v-3 } from './isomorphic-lib-v3';
-import { Isomorphic-lib-v-3 } from './isomorphic-lib-v3';
-undefined
