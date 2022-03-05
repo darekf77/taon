19120d67c7d976d563ad797ccc2a4d13c872a1da
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-03-05 16:01:29 +0100

    update

--- projects/container-v3/workspace-v3/isomorphic-lib-v3/app/src/app/app.module.ts
+++ projects/container-v3/workspace-v3/isomorphic-lib-v3/app/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     //<<<TO_REPLACE_MODULE>>>
   ],
   providers: [],
