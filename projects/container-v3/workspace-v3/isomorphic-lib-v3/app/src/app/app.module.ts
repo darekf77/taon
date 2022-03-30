571e136ba0c4e5f31481c3f96e8354ea56f264f7
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-03-30 03:10:17 +0200

    update

--- projects/container-v3/workspace-v3/isomorphic-lib-v3/app/src/app/app.module.ts
+++ projects/container-v3/workspace-v3/isomorphic-lib-v3/app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       //bundleOnly// or after 30 seconds (whichever comes first).
       //bundleOnlyregistrationStrategy: 'registerWhenStable:30000'
     //bundleOnly}),
+    BrowserAnimationsModule,
     //<<<TO_REPLACE_MODULE>>>
   ],
   providers: [],
