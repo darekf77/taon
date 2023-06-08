18992dd0c206092cbab5e4fe5adac19cf1e1795e
Dariusz Filipiak <darekf77@gmail.com>
2023-06-08 14:34:47 +0200

    v4 fixes

---
+++ projects/single-file-project-v4/environment.js
@@ -0,0 +1,11 @@
+
+const path = require('path')
+var { config } = { config: {} };
+
+config = {
+
+    domain: 'single-file-project-v2.example.domain.com',
+
+
+}
+module.exports = exports = { config };
