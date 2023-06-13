0e6d08a03ae3a5145f6138aa95db1b99f83b686e
Dariusz Filipiak <dariusz.filipiak@igt.com>
2023-06-13 09:09:03 +0200

    standaloen

--- projects/container-v4/gen-examples/firedev-backend-frontend-module/my-entity/index.ts
+++ projects/container-v4/gen-examples/firedev-backend-frontend-module/my-entity/index.ts
@@ -1,7 +1,4 @@
 export * from './my-entity';
-//#region @browser
-export * from './my-entity.module';
-//#endregion
 export * from './my-entity.models';
 export * from './my-entity.controller';
 //#region @browser
--- projects/container-v4/gen-examples/firedev-backend-frontend-module/my-entity/my-entity.component.ts
+++ projects/container-v4/gen-examples/firedev-backend-frontend-module/my-entity/my-entity.component.ts
@@ -1,12 +1,21 @@
 //#region @browser
 import { Component, OnInit } from '@angular/core';
 import { MyEntityService } from './my-entity.service';
+import { CommonModule } from '@angular/common';
+import { FiredevFullMaterialModule } from 'firedev-ui';
+import { StaticColumnsModule } from 'static-columns';
 
 @Component({
   selector: 'app-my-entity',
   templateUrl: './my-entity.component.html',
   styleUrls: ['./my-entity.component.scss'],
-  providers: [MyEntityService]
+  providers: [MyEntityService],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FiredevFullMaterialModule,
+    StaticColumnsModule,
+  ],
 })
 export class MyEntityComponent implements OnInit {
 
