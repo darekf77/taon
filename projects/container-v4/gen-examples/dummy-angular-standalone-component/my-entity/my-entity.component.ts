3c845918e05af674a534823a31e340b18eff63a9
Dariusz Filipiak <darekf77@gmail.com>
2024-02-04 01:42:43 +0100

    update

--- projects/container-v4/gen-examples/dummy-angular-standalone-component/my-entity/my-entity.component.ts
+++ projects/container-v4/gen-examples/dummy-angular-standalone-component/my-entity/my-entity.component.ts
@@ -1,25 +1,17 @@
 import { Component, EventEmitter, HostBinding, Input, OnInit, Output } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
-// import { _ } from 'tnp-core';
+import { _ } from 'tnp-core';
 
 @Component({
   selector: 'app-my-entity',
   templateUrl: './my-entity.component.html',
   styleUrls: ['./my-entity.component.scss'],
-  imports:[
+  imports: [
     CommonModule,
   ],
   standalone: true,
 })
-export class MyEntityComponent implements OnInit {
+export class MyEntityComponent {
 
-  @HostBinding('style.minHeight.px') @Input() height: number = 100;
-  @Output() myEntityDataChanged = new EventEmitter();
-  @Input() myEntityData: any = {};
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 }
