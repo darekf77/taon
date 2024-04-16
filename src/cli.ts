5bcea29b682ea544a2a87ec0102bde8ddd89b8f7
Dariusz Filipiak <darekf77@gmail.com>
2024-04-16 20:07:23 +0200

    release:  v16.5.27

---
+++ src/cli.ts
@@ -0,0 +1,14 @@
+let tnpStart = require('tnp/cli/src').start;
+//#region @notForNpm
+let tnpStartLocal = require('tnp/src').start;
+//#endregion
+
+let startFn = tnpStart;
+//#region @notForNpm
+startFn = tnpStartLocal; // locally I wanna use compiled tnp code / inside minified versions
+//#endregion
+
+export const start = async (args, frameworkName, mode) => {
+  await (startFn as any)(args, frameworkName, mode);
+}
+// DONT CHANGE THIS FILE!
