c7c51e20df74048e87210c69300e451b1ab7333b
Dariusz Filipiak <darekf77@gmail.com>
2024-07-06 09:09:29 +0200

    chore: inject subscribier

---
+++ projects/examples/subscriber-inject/environment.js
@@ -0,0 +1,16 @@
+
+const path = require('path')
+    var { config } = { config: {} };
+
+    config = {
+
+      domain: 'subscriber-inject.example.domain.com',
+      useDomain: false,
+      title: 'Subscriber Inject',
+      pwa: {
+        // start_url: ''
+      }
+
+    }
+    module.exports = exports = { config };
+    
