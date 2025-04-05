58a8a4d1da2473f01eb6082e3e97399778dd406a
Dariusz Filipiak <darekf77@gmail.com>
2025-04-05 22:08:02 +0200

    refactor: env options

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'taon.example.domain.com',
+    title: 'Taon',
+  },
+};
+export default env;
