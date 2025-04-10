3429d315524fed666c14eb8316e5c846a8a2cf2f
Dariusz Filipiak <darekf77@gmail.com>
2025-04-11 01:00:03 +0200

    chore: deps / env update

---
+++ projects/container-v18/env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'container-v18.example.domain.com',
+    title: 'Container V 18',
+  },
+};
+export default env;
---
+++ projects/container-v18/examples-v18/env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'examples-v18.example.domain.com',
+    title: 'Examples V 18',
+  },
+};
+export default env;
