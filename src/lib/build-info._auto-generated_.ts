431f45eebe86efb456ac921fb85ff02f1401f1c8
Dariusz Filipiak <darekf77@gmail.com>
2025-06-22 18:12:05 +0200

    release: v19.0.39

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -2,6 +2,6 @@
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'dev.taon.app';
 export const PROJECT_NPM_NAME = 'taon';
-export const CURRENT_PACKAGE_VERSION = '19.0.37';
+export const CURRENT_PACKAGE_VERSION = '19.0.39';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
