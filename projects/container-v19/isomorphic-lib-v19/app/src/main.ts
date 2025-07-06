0deb97a3e7b46d4cd913ed637ab0d035f805d34e
Dariusz Filipiak <darekf77@gmail.com>
2025-07-06 14:56:43 +0200

    feat: separated sqljs init

--- projects/container-v19/isomorphic-lib-v19/app/src/main.ts
+++ projects/container-v19/isomorphic-lib-v19/app/src/main.ts
@@ -1,45 +1,10 @@
-import { environment } from './environments/environment';
-const basename = '<<<TO_REPLACE_BASENAME>>>';
-import * as localForge from 'localforage';
-// @ts-ignore
-window['localforage'] = localForge;
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Helpers } from 'tnp-core'; // <- this is to replace by taon
-// import { TaonAdmin } from 'taon';  // <- this is to replace by taon
-import { Stor } from 'taon-storage'; // <- this is to replace by taon
 import { AppModule } from './app/app.module';
-
-if (environment.production) {
-  enableProdMode();
-}
+import { loadSqlJs } from './sqljs-loader';
 
 async function init() {
-  if (Helpers.isWebSQL) {
-    // @ts-ignore
-    const { default: initSqlJs } = await import('sql.js');
-    // or if you are in a browser:
-    // const initSqlJs = window.initSqlJs;
-
-    const SQL = await initSqlJs({
-      // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
-      // You can omit locateFile completely when running in node
-      // @ts-ignore
-      locateFile: file => {
-        const wasmPath = `${window.location.origin}${basename}assets/${file}`;
-        // console.log(`Trying to get sql.js wasm from: ${wasmPath}`)
-        return wasmPath;
-        // return `https://sql.js.org/dist/${file}`;
-      },
-    });
-
-    // @ts-ignore
-    window['SQL'] = SQL;
-    console.log('WEBSQL LOADED');
-  } else {
-    console.log('WEBSQL NOT LOADED');
-  }
-  await Stor.awaitPendingOperatios();
+  await loadSqlJs();
   platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .catch(err => console.error(err));
