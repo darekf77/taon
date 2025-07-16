e7d9d8d53965995ec149194b4ca0d6ac74895ebc
Dariusz Filipiak <darekf77@gmail.com>
2025-07-16 10:31:54 +0200

    fix: proper file for new cli

--- projects/container-v19/isomorphic-lib-v19/src/index.ts
+++ projects/container-v19/isomorphic-lib-v19/src/index.ts
@@ -1,9 +1,4 @@
 export * from './lib';
-
 //#region @backend
-export async function run(args: string[]) {
-  console.log('Hello world from CLI!');
-  process.exit(0);
-}
-export default run;
+export { run } from './cli';
 //#endregion
