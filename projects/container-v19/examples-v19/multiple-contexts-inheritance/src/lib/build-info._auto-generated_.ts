7feacc35d2c2424ac86e693fb83cfe41952fdc89
Dariusz Filipiak <darekf77@gmail.com>
2025-08-09 09:10:35 +0200

    chore: update project multiple contexts inheritance

--- projects/container-v19/examples-v19/multiple-contexts-inheritance/src/lib/build-info._auto-generated_.ts
+++ projects/container-v19/examples-v19/multiple-contexts-inheritance/src/lib/build-info._auto-generated_.ts
@@ -1,6 +1,7 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'com.domain.example.multiple-contexts-inheritance';
+export const BUILD_BASE_HREF = '';
 export const PROJECT_NPM_NAME = 'multiple-contexts-inheritance';
 export const CURRENT_PACKAGE_VERSION = '0.0.0';
 // THIS FILE IS GENERATED - DO NOT MODIFY
