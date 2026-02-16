1bf2fa6791a7c43fb9952e04f4abfe36d7e31b20
Dariusz Filipiak <darekf77@gmail.com>
2026-02-16 14:43:23 +0100

    chore: update

--- src/app.vscode.ts
+++ src/app.vscode.ts
@@ -1,44 +1,44 @@
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-/* */
-  
+//#region @notForNpm
+import { vscodeExtMethods, activateMenuTnp, deactivateMenuTnp } from 'tnp/src';
+import { executeCommand } from 'tnp-helpers/src'; // @backend
+import type { ExtensionContext } from 'vscode';
+const FRAMEWORK_NAME = 'taon';
+
+const commands = vscodeExtMethods(FRAMEWORK_NAME);
+export async function activate(context: ExtensionContext) {
+  //#region @backendFunc
+  const vscode = await import('vscode');
+  activateMenuTnp(context, vscode, FRAMEWORK_NAME);
+
+  for (let index = 0; index < commands.length; index++) {
+    const {
+      title,
+      command = '',
+      exec = '',
+      options,
+      isDefaultBuildCommand,
+    } = commands[index];
+    const sub = executeCommand(
+      title,
+      command,
+      // @ts-ignore
+      exec,
+      options,
+      isDefaultBuildCommand,
+      context,
+    );
+    if (sub) {
+      context.subscriptions.push(sub);
+    }
+  }
+  //#endregion
+}
+
+export function deactivate() {
+  //#region @backendFunc
+  deactivateMenuTnp();
+  //#endregion
+}
+
+export default { commands };
+//#endregion
